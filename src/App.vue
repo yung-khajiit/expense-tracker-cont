a2834feab48274bc121ac089077252c9c7c271ce
yung-khajiit <jeenriquez1219@gmail.com>
2024-02-03 11:01:17 -0600

    Added local storage

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   import IncomeExpenses from './components/IncomeExpenses.vue';
   import AddTransaction from './components/AddTransaction.vue';
   import TransactionList from './components/TransactionList.vue';
-  import {ref, computed} from 'vue'
+  import {ref, computed, onMounted} from 'vue'
 
   const transactions = ref([])
 
@@ -48,6 +48,8 @@
       text: transactionData.text,
       amount: transactionData.amount,
     })
+
+    saveTransactionToLocalStorage()
   }
 
   //generate unique id
@@ -58,7 +60,23 @@
   //delete transaction
   const handleTransactionDeleted = (id) => {
     transactions.value = transactions.value.filter((transaction) => transaction.id !== id)
+    saveTransactionToLocalStorage()
   }
+
+  //save to local storage
+  const saveTransactionToLocalStorage = () => {
+    localStorage.setItem('transactions', JSON.stringify(transactions.value))
+  }
+
+  //first loads
+  onMounted(() => {
+    const savedTransactions = JSON.parse(localStorage.getItem('transactions'))
+
+    if(savedTransactions)
+    {
+      transactions.value = savedTransactions
+    }
+  })
 
 </script>
 
