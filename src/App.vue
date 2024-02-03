352af065d3d5f66f6dcc39bb209d272cee1e0a24
yung-khajiit <jeenriquez1219@gmail.com>
2024-02-03 10:52:04 -0600

    Delete Function is working

--- src/App.vue
+++ src/App.vue
@@ -55,6 +55,11 @@
     return Math.floor(Math.random() * 10000000)
   }
 
+  //delete transaction
+  const handleTransactionDeleted = (id) => {
+    transactions.value = transactions.value.filter((transaction) => transaction.id !== id)
+  }
+
 </script>
 
 <template>
